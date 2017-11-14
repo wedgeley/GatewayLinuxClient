0129636e12afd3e14f736926a3b699931fcab6ad
Phil <phil@ubtest804.(none)>
2017-11-14 12:08:17 +0000

    Project changes

--- GatewayTest/constants.c
+++ GatewayTest/constants.c
@@ -2,4 +2,4 @@
 
 const size_t KEYCODE_LENGTH = 12;           // Any key codes longer than this will cause an error
 const size_t KEYCODE_PAGE_SIZE = 4;         // Number of key codes to fetch at a time
-const int MAX_PAGES_TO_DISPLAY = 10;        // Never display more than this many pages
+const int MAX_PAGES_TO_DISPLAY = 50;        // Never display more than this many pages
